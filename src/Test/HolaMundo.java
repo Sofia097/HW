89eac5956f8c5e887993c206e984cafad9253a06
USUARIO <USUARIO@DESKTOP-13U8KIU>
2021-03-05 20:43:17 -0600

    version3

---
+++ src/Test/HolaMundo.java
@@ -0,0 +1,16 @@
+/*
+Ángela Sofía Peña Olvera.
+2022684
+ */
+package Test;
+
+public class HolaMundo {
+    
+    public static void main (String[] args){
+        
+        System.out.print("Bievenidos a la clase POO");
+        
+        //SOUT
+        //METACOMA
+    }
+}
